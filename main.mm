306bc171f8ba50c2cbd58d9985ef2dbfeaf91f17
Subho Halder <subho.halder@gmail.com>
2021-08-02 23:02:21 +0530

    log the arguments when passed

--- main.mm
+++ main.mm
@@ -6,6 +6,7 @@ int main(int argc, char **argv, char **envp) {
 	if (argc > 1) {
 		NSString *host = [NSString stringWithUTF8String:argv[1]];
 		NSString *port = [NSString stringWithUTF8String:argv[2]];
+		NSLog(@"Proxy -- %@ : %d", host, [port intValue]);
 		[proxy setProxy:host port:[port intValue] shouldEnableProxy:YES];
 	} else {
 		[proxy setProxy:@"" port:0 shouldEnableProxy:NO];
